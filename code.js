6f47d4e277c6ecc9682041a9d098109eeca7e4c1
kendric84 <graham.k.smith@live.com>
2020-08-12 19:57:27 -0400

    Question-session

--- code.js
+++ code.js
@@ -1,52 +1,165 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var response = JSON.parse(xhttp.responseText);
-    var pendingTasks = response.pendingTasks;
+//Declare variables
+let pendingTasks;
+let pendingTaskDates;
+let completedTasks;
+let completedTaskDates;
+let completedTaskDueDates;
 
-    var pendingDescriptionOutput = "";
-    var pendingDueDateOutput = "";
-    var checkMark = "";
-    for (var i = 0; i < pendingTasks.length; i++) {
-      pendingDescriptionOutput +=
-        "<li>" + pendingTasks[i].description + "</li>";
-    }
-    for (var i = 0; i < pendingTasks.length; i++) {
-      pendingDueDateOutput += "<li>" + pendingTasks[i].dueDate + "</li>";
-      checkMark += "<li>&#10004</li>";
-    }
+//Pull data from local storage
+function pullData() {
+  localStorage.getItem("localPendingTasks")
+    ? (pendingTasks = JSON.parse(localStorage.getItem("localPendingTasks")))
+    : (pendingTasks = []);
 
-    document.getElementById("pendingTasks").innerHTML = pendingDescriptionOutput;
-    document.getElementById("pendingDueDate").innerHTML = pendingDueDateOutput;
+  localStorage.getItem("localPendingTaskDates")
+    ? (pendingTaskDates = JSON.parse(
+        localStorage.getItem("localPendingTaskDates")
+      ))
+    : (pendingTaskDates = []);
 
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(xhttp.responseText);
-      var completedTasks = response.completedTasks;
+  localStorage.getItem("localCompletedTasks")
+    ? (completedTasks = JSON.parse(localStorage.getItem("localCompletedTasks")))
+    : (completedTasks = []);
 
-      var completedDescriptionOutput = "";
-      var completedDueDateOutput = "";
-      var completedDateOutput = "";
-      for (var i = 0; i < completedTasks.length; i++) {
-        completedDescriptionOutput +=
-          "<li>" + completedTasks[i].description + "</li>";
-      }
+  localStorage.getItem("localCompletedTaskDates")
+    ? (completedTaskDates = JSON.parse(
+        localStorage.getItem("localCompletedTaskDates")
+      ))
+    : (completedTaskDates = []);
 
-      for (var i = 0; i < completedTasks.length; i++) {
-        completedDueDateOutput +=
-          "<li>" + completedTasks[i].dueDate + "</li>";
-      }
+  localStorage.getItem("localCompletedTaskDueDates")
+    ? (completedTaskDueDates = JSON.parse(
+        localStorage.getItem("localCompletedTaskDueDates")
+      ))
+    : (completedTaskDueDates = []);
+}
 
-      for (var i = 0; i < completedTasks.length; i++) {
-        completedDateOutput +=
-          "<li>" + completedTasks[i].completedDate + "</li>";
-      }
+//Push data to local storage
+function pushData() {
+  localStorage.clear();
+  localStorage.setItem("localPendingTasks", JSON.stringify(pendingTasks));
+  localStorage.setItem(
+    "localPendingTaskDates",
+    JSON.stringify(pendingTaskDates)
+  );
+  localStorage.setItem("localCompletedTasks", JSON.stringify(completedTasks));
+  localStorage.setItem(
+    "localCompletedTaskDates",
+    JSON.stringify(completedTaskDates)
+  );
+  localStorage.setItem(
+    "localCompletedTaskDueDates",
+    JSON.stringify(completedTaskDueDates)
+  );
+}
 
-      document.getElementById("completedTasks").innerHTML = completedDescriptionOutput;
-      document.getElementById("completedDueDate").innerHTML = completedDueDateOutput;
-      document.getElementById("completedDate").innerHTML = completedDateOutput;
-      document.getElementById("markComplete").innerHTML = checkMark;
-    }
+//Add a new task
+function addTask() {
+  pendingTasks.push(document.getElementById("new-task-text").value);
+  pendingTaskDates.push(document.getElementById("new-task-date").value);
+}
+
+//Refresh pending and completed lists
+function refreshLists() {
+  let pendingDescriptionOutput = "";
+  let pendingDueDateOutput = "";
+  let completedDescriptionOutput = "";
+  let completedDueDateOutput = "";
+  let completedDateOutput = "";
+  let checkMark = "";
+
+  for (let i = 0; i < pendingTasks.length; i++) {
+    // pendingDescriptionOutput += "<li>" + pendingTasks[i] + "</li>";
+    let ol = document.getElementById("pending-tasks");
+    let li = document.createElement("li");
+    let newText = pendingTasks[i];
+    //   li.setAttribute('id',i);
+    li.appendChild(document.createTextNode(newText));
+    ol.appendChild(li);
+    console.log(ol);
   }
-};
-xhttp.open("GET", "tasks.json", true);
-xhttp.send();
+
+  for (let i = 0; i < pendingTaskDates.length; i++) {
+    pendingDueDateOutput += "<li>" + pendingTaskDates[i] + "</li>";
+    checkMark += '<li class="check-mark-list" id="' + i + '">&#10004</li>';
+  }
+
+  for (let i = 0; i < completedTasks.length; i++) {
+    completedDescriptionOutput += "<li>" + completedTasks[i] + "</li>";
+  }
+
+  for (let i = 0; i < completedTaskDates.length; i++) {
+    completedDateOutput += "<li>" + completedTaskDates[i] + "</li>";
+  }
+
+  for (let i = 0; i < completedTaskDueDates.length; i++) {
+    completedDueDateOutput += "<li>" + completedTaskDueDates[i] + "</li>";
+  }
+
+  pendingDescriptionOutput != null
+    ? (document.getElementById(
+        "pending-tasks"
+      ).innerHTML = pendingDescriptionOutput)
+    : void 0;
+  pendingDueDateOutput != null
+    ? (document.getElementById(
+        "pending-due-date"
+      ).innerHTML = pendingDueDateOutput)
+    : void 0;
+  completedDescriptionOutput != null
+    ? (document.getElementById(
+        "completed-tasks"
+      ).innerHTML = completedDescriptionOutput)
+    : void 0;
+  completedDateOutput != null
+    ? (document.getElementById(
+        "completed-date"
+      ).innerHTML = completedDateOutput)
+    : void 0;
+  completedDueDateOutput != null
+    ? (document.getElementById(
+        "completed-due-date"
+      ).innerHTML = completedDueDateOutput)
+    : void 0;
+  checkMark != null
+    ? (document.getElementById("mark-complete").innerHTML = checkMark)
+    : void 0;
+}
+
+//Add Task button event
+const addButton = document.getElementById("new-task-button");
+addButton.addEventListener("click", function () {
+  addTask();
+  pushData();
+  refreshLists();
+});
+
+//Clear button event
+const clearCompletedButton = document.getElementById("clear-button");
+addButton.addEventListener("click", function () {
+  completedTasks = "";
+  completedTaskDates = "";
+  completedTaskDueDates = "";
+  localStorage.removeItem("localCompletedTasks");
+  localStorage.removeItem("localCompletedTaskDates");
+  localStorage.removeItem("localCompletedTaskDueDates");
+  refreshLists();
+});
+
+//Mark complete event
+document.querySelectorAll(".check-mark-list").forEach((item) => {
+  item.addEventListener("click", function () {
+    console.log("Working!");
+  });
+});
+
+/*
+const markCompleteClick = document.getElementById("mark-complete");
+markCompleteClick.addEventListener("click", function () {
+  pushData();
+  refreshLists();
+});
+*/
+
+pullData();
+refreshLists();
