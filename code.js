831130e362a40453dfb57140f99b7153689a91eb
kendric84 <graham.k.smith@live.com>
2020-08-13 20:34:42 -0400

    Code Review

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 //Declare variables
 let pendingTasks;
-let pendingTaskDates;
+let pendingDueDates;
 let completedTasks;
 let completedTaskDates;
 let completedTaskDueDates;
@@ -11,11 +11,11 @@ function pullData() {
     ? (pendingTasks = JSON.parse(localStorage.getItem("localPendingTasks")))
     : (pendingTasks = []);
 
-  localStorage.getItem("localPendingTaskDates")
-    ? (pendingTaskDates = JSON.parse(
-        localStorage.getItem("localPendingTaskDates")
+  localStorage.getItem("localPendingDueDates")
+    ? (pendingDueDates = JSON.parse(
+        localStorage.getItem("localPendingDueDates")
       ))
-    : (pendingTaskDates = []);
+    : (pendingDueDates = []);
 
   localStorage.getItem("localCompletedTasks")
     ? (completedTasks = JSON.parse(localStorage.getItem("localCompletedTasks")))
@@ -38,10 +38,7 @@ function pullData() {
 function pushData() {
   localStorage.clear();
   localStorage.setItem("localPendingTasks", JSON.stringify(pendingTasks));
-  localStorage.setItem(
-    "localPendingTaskDates",
-    JSON.stringify(pendingTaskDates)
-  );
+  localStorage.setItem("localPendingDueDates", JSON.stringify(pendingDueDates));
   localStorage.setItem("localCompletedTasks", JSON.stringify(completedTasks));
   localStorage.setItem(
     "localCompletedTaskDates",
@@ -55,75 +52,114 @@ function pushData() {
 
 //Add a new task
 function addTask() {
-  pendingTasks.push(document.getElementById("new-task-text").value);
-  pendingTaskDates.push(document.getElementById("new-task-date").value);
+  if (document.getElementById("new-task-text").value == "" || document.getElementById("new-task-date").value == "") {
+    alert("Enter a task and date to save")
+  }
+  else {
+    pendingTasks.push(document.getElementById("new-task-text").value);
+    document.getElementById("new-task-text").value = "";
+    pendingDueDates.push(document.getElementById("new-task-date").value);
+    document.getElementById("new-task-date").value = "";
+  }  
 }
 
 //Refresh pending and completed lists
 function refreshLists() {
-  let pendingDescriptionOutput = "";
-  let pendingDueDateOutput = "";
-  let completedDescriptionOutput = "";
-  let completedDueDateOutput = "";
-  let completedDateOutput = "";
   let checkMark = "";
 
+  //Clear existing column entries
+  let ol = document.getElementById("pending-tasks");
+  while (ol.lastChild) {
+    ol.removeChild(ol.lastChild);
+  }
+  //Repopulate column
   for (let i = 0; i < pendingTasks.length; i++) {
-    // pendingDescriptionOutput += "<li>" + pendingTasks[i] + "</li>";
-    let ol = document.getElementById("pending-tasks");
     let li = document.createElement("li");
     let newText = pendingTasks[i];
-    //   li.setAttribute('id',i);
+    li.setAttribute("id", i);
     li.appendChild(document.createTextNode(newText));
     ol.appendChild(li);
-    console.log(ol);
   }
 
-  for (let i = 0; i < pendingTaskDates.length; i++) {
-    pendingDueDateOutput += "<li>" + pendingTaskDates[i] + "</li>";
+  //Clear existing column entries
+  ol = document.getElementById("pending-due-date");
+  while (ol.lastChild) {
+    ol.removeChild(ol.lastChild);
+  }
+  //Repopulate column
+  for (let i = 0; i < pendingDueDates.length; i++) {
+    let li = document.createElement("li");
+    let newText = pendingDueDates[i];
+    li.setAttribute("id", i);
+    li.appendChild(document.createTextNode(newText));
+    ol.appendChild(li);
+
+    // I left this in because I couldn't make the appendChild approach work with the UTF code
     checkMark += '<li class="check-mark-list" id="' + i + '">&#10004</li>';
   }
 
+  document.getElementById("mark-complete").innerHTML = checkMark;
+
+  /* This dosen't work, the UTF code shows as a text string
+  for (let i = 0; i < pendingDueDates.length; i++) {
+    let ol = document.getElementById("mark-complete");
+    let li = document.createElement("li");
+    let newText = "&#10004";
+    li.setAttribute('id',i);
+    li.setAttribute('class',"check-mark-list");
+    li.appendChild(document.createTextNode(newText));
+    ol.appendChild(li);
+  }
+*/
+  //Clear existing column entries
+  ol = document.getElementById("completed-tasks");
+  while (ol.lastChild) {
+    ol.removeChild(ol.lastChild);
+  }
+  //Repopulate column
   for (let i = 0; i < completedTasks.length; i++) {
-    completedDescriptionOutput += "<li>" + completedTasks[i] + "</li>";
+    let li = document.createElement("li");
+    let newText = completedTasks[i];
+    li.setAttribute("id", i);
+    li.appendChild(document.createTextNode(newText));
+    ol.appendChild(li);
   }
 
+  //Clear existing column entries
+  ol = document.getElementById("completed-due-date");
+  while (ol.lastChild) {
+    ol.removeChild(ol.lastChild);
+  }
+  //Repopulate column
   for (let i = 0; i < completedTaskDates.length; i++) {
-    completedDateOutput += "<li>" + completedTaskDates[i] + "</li>";
+    let li = document.createElement("li");
+    let newText = completedTaskDates[i];
+    li.setAttribute("id", i);
+    li.appendChild(document.createTextNode(newText));
+    ol.appendChild(li);
   }
 
+  //Clear existing column entries
+  ol = document.getElementById("completed-date");
+  while (ol.lastChild) {
+    ol.removeChild(ol.lastChild);
+  }
+  //Repopulate column
   for (let i = 0; i < completedTaskDueDates.length; i++) {
-    completedDueDateOutput += "<li>" + completedTaskDueDates[i] + "</li>";
+    let li = document.createElement("li");
+    let newText = completedTaskDueDates[i];
+    li.setAttribute("id", i);
+    li.appendChild(document.createTextNode(newText));
+    ol.appendChild(li);
   }
 
-  pendingDescriptionOutput != null
-    ? (document.getElementById(
-        "pending-tasks"
-      ).innerHTML = pendingDescriptionOutput)
-    : void 0;
-  pendingDueDateOutput != null
-    ? (document.getElementById(
-        "pending-due-date"
-      ).innerHTML = pendingDueDateOutput)
-    : void 0;
-  completedDescriptionOutput != null
-    ? (document.getElementById(
-        "completed-tasks"
-      ).innerHTML = completedDescriptionOutput)
-    : void 0;
-  completedDateOutput != null
-    ? (document.getElementById(
-        "completed-date"
-      ).innerHTML = completedDateOutput)
-    : void 0;
-  completedDueDateOutput != null
-    ? (document.getElementById(
-        "completed-due-date"
-      ).innerHTML = completedDueDateOutput)
-    : void 0;
-  checkMark != null
-    ? (document.getElementById("mark-complete").innerHTML = checkMark)
-    : void 0;
+  //Set or reset mark complete click events
+  let items = document.getElementsByClassName("check-mark-list");
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function () {
+      markComplete(items[i].id);
+    });
+  }
 }
 
 //Add Task button event
@@ -134,32 +170,43 @@ addButton.addEventListener("click", function () {
   refreshLists();
 });
 
-//Clear button event
-const clearCompletedButton = document.getElementById("clear-button");
-addButton.addEventListener("click", function () {
-  completedTasks = "";
-  completedTaskDates = "";
-  completedTaskDueDates = "";
+//Clear completed button click event
+const clearCompletedButton = document.getElementById("clear-completed-button");
+clearCompletedButton.addEventListener("click", function () {
+  completedTasks = [];
+  completedTaskDates = [];
+  completedTaskDueDates = [];
   localStorage.removeItem("localCompletedTasks");
   localStorage.removeItem("localCompletedTaskDates");
   localStorage.removeItem("localCompletedTaskDueDates");
   refreshLists();
 });
 
-//Mark complete event
-document.querySelectorAll(".check-mark-list").forEach((item) => {
-  item.addEventListener("click", function () {
-    console.log("Working!");
-  });
-});
-
-/*
-const markCompleteClick = document.getElementById("mark-complete");
-markCompleteClick.addEventListener("click", function () {
-  pushData();
+//Clear all button click event
+const clearAllButton = document.getElementById("clear-all-button");
+clearAllButton.addEventListener("click", function () {
+  localStorage.clear();
+  pendingTasks = [];
+  pendingDueDates = [];
+  completedTasks = [];
+  completedTaskDates = [];
+  completedTaskDueDates = [];
   refreshLists();
 });
-*/
 
 pullData();
 refreshLists();
+
+//Mark a single item complete and move from pending to complete list
+function markComplete(completedItemId) {
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  completedTasks.push(pendingTasks[completedItemId]);
+  completedTaskDueDates.push(pendingDueDates[completedItemId]);
+  completedTaskDates.push(date);
+  pendingTasks.splice(completedItemId, 1);
+  pendingDueDates.splice(completedItemId, 1);
+  pushData();
+  refreshLists();
+}
